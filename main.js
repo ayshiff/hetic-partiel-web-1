17aa0bd8b2a1972deb30f821def6b0b78625bed7
ayshiff <remi.d45@gmail.com>
2018-06-14 12:47:52 +0200

    Issue 2

--- main.js
+++ main.js
@@ -4,21 +4,52 @@ let closeSelector = document.querySelector('.close');
 let backToTheTopSelector = document.querySelector('.backToTheTop');
 
 burgerContainerSelector.addEventListener('click', () => {
-    burgerMenuSelector.style.display = "block";
+	burgerMenuSelector.style.display = "block";
 });
 
 closeSelector.addEventListener('click', () => {
-    burgerMenuSelector.style.display = "none";
+	burgerMenuSelector.style.display = "none";
 });
+
 const scrollToTop = () => {
-	if (document.body.scrollTop!=0 || document.documentElement.scrollTop!=0){
-		window.scrollBy(0,-50);
-		timeOut=setTimeout('scrollToTop()',10);
-	}
-	else clearTimeout(timeOut);
+	if (document.body.scrollTop != 0 || document.documentElement.scrollTop != 0) {
+		window.scrollBy(0, -50);
+		timeOut = setTimeout('scrollToTop()', 10);
+	} else clearTimeout(timeOut);
 }
 
 backToTheTopSelector.addEventListener('click', () => {
-    scrollToTop();
+	scrollToTop();
 });
 
+
+window.onscroll = function () {
+	Scroll()
+};
+
+var navbar = document.querySelector('.row1');
+
+var sticky = navbar.offsetTop;
+
+function Scroll() {
+	var st = window.pageYOffset || document.documentElement.scrollTop;
+	if (window.pageYOffset >= sticky) {
+		navbar.classList.add("sticky")
+	} else {
+		navbar.classList.remove("sticky");
+	}
+
+	if (st > lastScrollTop) {
+		document.getElementById(".sticky").style.display = "none"
+	} else {
+		document.getElementById(".sticky").style.display = "block"
+	}
+	lastScrollTop = st;
+}
+
+var lastScrollTop = 0;
+
+window.addEventListener("scroll", function () {
+
+
+}, false);
